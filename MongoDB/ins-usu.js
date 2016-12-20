90c4791e6996023561c1a299a4afbc2f7aba8116
SOFTTEK\oscar.samoilovich <oscar.samoilovich@softtek.com>
2016-12-20 14:44:04 -0300

    Scripts de insert Collections: accion-usuario-reserva. No hace falta CREATE previo

---
+++ MongoDB/ins-usu.js
@@ -0,0 +1,14 @@
+db.usuario.drop()
+
+db.usuario.insert({idU: NumberInt(1),NyA:"Sofia Marina Diaz", Cel : NumberInt(1512345678), Sim : null, Log : ISODate("2016-12-06"),Tok :null,Pwd:"123",Mai:"null"})
+db.usuario.insert({idU: NumberInt(2),NyA:"Raquel Estela Castro", Cel : NumberInt(null), Sim : null, Log : ISODate("2016-12-06 12:34:56"),Tok :null,Pwd:"123",Mai:"raquel.castro@softtek.com"})
+db.usuario.insert({idU: NumberInt(3),NyA:"Maria Florencia Inda", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"oscar.samoilovich@softtek.com"})
+db.usuario.insert({idU: NumberInt(4),NyA:"Ignacio Novio", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"null"})
+db.usuario.insert({idU: NumberInt(5),NyA:"Oscar Samoilovich", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"oscar.samoilovich@softtek.com"})
+db.usuario.insert({idU: NumberInt(6),NyA:"Guillermo Goldar", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"guillermo.goldar@softtek.com"})
+db.usuario.insert({idU: NumberInt(7),NyA:"Manuel Alvarez Lojo", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"null"})
+db.usuario.insert({idU: NumberInt(8),NyA:"Leonardo Jerez", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"null"})
+db.usuario.insert({idU: NumberInt(9),NyA:"Nicolas  Fernandez", Cel : NumberInt(null), Sim : null, Log : ISODate(null),Tok :null,Pwd:"123",Mai:"nicolas.fernandez@softtek.com"})
+
+db.usuario.find()
+ 
