e1d2606c20564a8f2e6ccc032ea7eb34cc3bf2b2
Nof84 <nof84@icloud.com>
2016-12-26 12:20:16 -0300

    Se actualiza el front end.

--- BackEnd/models/garage.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var garageSchema = new Schema({
-    garage:       { type: String },
-    estado:       { type: String }
-});
-
-module.exports = mongoose.model('Garage', garageSchema);
